fbc369b67a3095e06c3f9825bb2d650db148ff6a
wty0906 <wtyu1997@163.com>
2022-04-01 22:51:41 +0800

    创建目录

---
+++ 第一章/src/com/diyizhang/HelloWord.java
@@ -0,0 +1,11 @@
+package com.diyizhang;
+
+/**
+ * @author ty.wang
+ * @since 2022/4/122:50
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
+}
