6167ae634b7336344da81b0bc1bf9d334d5cbc29
wangtianyu53 <wangtianyu53@jd.com>
2022-07-04 20:30:42 +0800

    多线程

---
+++ 第十三章/src/createThread/Test.java
@@ -0,0 +1,22 @@
+package createThread;
+
+/**
+ * @author ty.wang
+ * @since 2022/7/415:55
+ */
+public class Test {
+    public static void main(String[] args) {
+        Thread.currentThread().setName("主线程");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(Thread.currentThread().getName()+"1--" + i);
+        }
+        //制造其他线程，要主线程争抢资源
+        //具体的线程对象
+        CreateThread01 createThread01 = new CreateThread01("子线程");
+        createThread01.start();
+        //主线程中也要输出1-10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(Thread.currentThread().getName()+"2---" + i);
+        }
+    }
+}
---
+++ 第十三章/src/createThread/lianxi/BuyTicketTest.java
@@ -0,0 +1,21 @@
+package createThread.lianxi;
+
+/**
+ * @author ty.wang
+ * @since 2022/7/416:57
+ */
+public class BuyTicketTest {
+    public static void main(String[] args) {
+        BuyTicketThread thread1 = new BuyTicketThread();
+        thread1.setName("窗口1");
+        BuyTicketThread thread2 = new BuyTicketThread();
+        thread2.setName("窗口2");
+        BuyTicketThread thread3 = new BuyTicketThread();
+        thread3.setName("窗口3");
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+    }
+}
